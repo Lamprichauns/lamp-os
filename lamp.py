8cea894178e452edd31610b9001097e056d37b87
Jerrett Strain <jerrett@5stone.io>
2021-10-14 18:32:23 -0700

    rename to lamp.py - will move to seperate lampname.py files soon, i think

--- lamp.py
+++ lamp.py
@@ -4,11 +4,6 @@ from time import sleep
 import network
 import re
 
-# Lamp Config
-
-lamp_name   = "womp"
-color       = {"base": '#00ff00', "shade": '#ffffff'}
-
 # Signal strength threshold - weaker signal than this doesn't count as "close".
 signal_threshold = -90
 
@@ -27,24 +22,7 @@ class Lamp:
         self.base_color = base_color
         self.shade_color = shade_color
 
-def setup():
-    global lamp_network, wifi_sta
-
-    lamp_network = {"current": [], "joined": [], "left": []}
-
-    # Init wifi
-    wifi_sta = network.WLAN(network.STA_IF)
-    wifi_ap  = network.WLAN(network.AP_IF)
-
-    wifi_ap.active(True)
-    wifi_sta.active(True)
-
-    # TODO: make this "LampOS-lampname-basecolor-shadecolor"
-    ssid = "LampOS-%s" % (lamp_name)
-    wifi_ap.config(essid=ssid, password="lamprichauns")
-
-    print("%s is awake!" % (lamp_name))
-
+# Scan networks and find lamps, and track lamps who
 def scan_networks():
     networks = wifi_sta.scan()
     nearby_lamps = []
@@ -79,23 +57,46 @@ def scan_networks():
     # Set current list
     lamp_network["current"] = nearby_lamps.copy()
 
-
+# Change the color of the shade and base if needed
 #def adjust_colors
   # Call callbacks base_color(), shade_color() to determine color, use config if callbacks are not defined.
   # if new color is not the same as last set color, set the new colors
 
-#####
 
-setup()
+# Setup this lamp upon boot.
+def setup():
+    global lamp_network, wifi_sta, config
+
+    # Default lamp settings. White/White and no name
+    config = Lamp("noname", "#ffffff", "#ffffff")
+    lamp_network = {"current": [], "joined": [], "left": []}
+
+    # Init wifi
+    wifi_sta = network.WLAN(network.STA_IF)
+    wifi_ap  = network.WLAN(network.AP_IF)
+
+    wifi_ap.active(True)
+    wifi_sta.active(True)
+
+    # TODO: make this "LampOS-lampname-basecolor-shadecolor"
+    ssid = "LampOS-%s" % (config.name)
+    wifi_ap.config(essid=ssid, password="lamprichauns")
+
+    print("%s is awake!" % (config.name))
 
 # Scan for lamps.
 # Making this too frequent could also result in unfavourably reactivity if a lamp
 # is on the edge and ebbing in and out of "nearness" - and scanning takes extra power!
 # Using virtual timer instead of hardware for compatibility with ESP8266.
-timer = Timer(-1)
-timer.init(period=10000, mode=Timer.PERIODIC, callback=lambda t:scan_networks())
+def run():
+    timer = Timer(-1)
+    timer.init(period=10000, mode=Timer.PERIODIC, callback=lambda t:scan_networks())
 
-# Loop pretty quickly, so we can do sub-second color changes if we want.
-while True:
-    # adjust_colors()
-    sleep(0.25)
+    # Loop pretty quickly, so we can do sub-second color changes if we want.
+    while True:
+        # adjust_colors()
+        sleep(0.25)
+
+
+setup()
+run()
