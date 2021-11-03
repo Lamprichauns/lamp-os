30811dc6e20b5972d8b3f7d0de2e90e518a5ee5b
Jerrett Strain <jerrett@5stone.io>
2021-11-03 10:15:01 -0700

    no longer using boot

--- tasks.py
+++ tasks.py
@@ -2,7 +2,6 @@ from invoke import task
 
 @task 
 def flash(c, port, lamp):
-    c.run(f"ampy --port {port} put ./src/boot.py boot.py")
     c.run(f"ampy --port {port} put ./src/lamp.py lamp.py")
     c.run(f"ampy --port {port} put ./src/lamps/{lamp}.py main.py")
     print(f"{lamp} flashed to %{port}")
