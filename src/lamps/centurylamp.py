2836449d341c8b8c40774c79448c90aa2fd8d342
Rich <chaffneue@gmail.com>
2022-04-28 00:53:05 -0700

    Updating century to use motion library. Added a quick and dirty algorithm from a paper. Added citation in the @see docblock stanza

--- src/lamps/centurylamp.py
+++ src/lamps/centurylamp.py
@@ -1,11 +1,28 @@
 from lamp import Lamp
+from behaviour import Behaviour
+from motion_6050 import LampMotionMPU6050
+import uasyncio as asyncio
 
 config = {
     "base": { "pin": 12, "pixels": 40 },
     "shade": { "pin": 13, "pixels": 40 },
-    "touch": { "pin": 32 },
-    "motion": { "pin_sda": 21, "pin_scl": 22, "gravity_axis": "x_axis" }
+    "motion": { "pin_sda": 21, "pin_scl": 22, "polling_interval": 10e-2 }
 }
 
+motion = LampMotionMPU6050(config["motion"]["pin_sda"], config["motion"]["pin_scl"])
+
+# Sample punter motion
+class MotionCaptureBehavior(Behaviour):
+    def measure(self):
+        print(motion.get_movement_intensity_value())
+
+    async def run(self):
+        polling_interval = config["motion"]["polling_interval"]
+        while True:
+            await asyncio.sleep(polling_interval)
+            async with self.lamp.lock:
+                self.measure()
+
 century = Lamp("century", "#5b4711", "#5b4711", config)
-century.wake();
+century.add_behaviour(MotionCaptureBehavior)
+century.wake()
--- src/motion_6050.py
+++ src/motion_6050.py
@@ -1,14 +1,20 @@
 from machine import I2C, Pin
+from math import sqrt, pow, floor
 import mpu6050
 
 # Abstraction for lamp motion using the onboard MPU-6050 accelerometer
-# To allow for different mounting options for your lamp pcb,
-# gravity_axis will inform the software what axis gravity is acting on
 class LampMotionMPU6050:
-    def __init__(self, pin_sda, pin_scl, gravity_axis):
-        self.gyro_plane = gyro_plane
-        i2c = I2C(sda=Pin(pin_sda), scl=Pin(pin_scl))
-        accelerometer = mpu6050.accel(i2c)
+    def __init__(self, pin_sda, pin_scl):
+        self.i2c = I2C(sda=Pin(pin_sda), scl=Pin(pin_scl))
+        self.accelerometer = mpu6050.accel(self.i2c)
+        self.previous_sample = 0.0;
 
-    def getValues():
-        return accelerometer.get_values()
+    # Get an unsigned movement intensity value over a monotonic sample period
+    # @see https://www.egr.msu.edu/~mizhang/papers/feature-selection-activity-recognition-bodynets11.pdf
+    def get_movement_intensity_value(self):
+        value = self.accelerometer.get_values();
+        norm = sqrt(pow(abs(value['AcZ']), 2) + pow(abs(value['AcY']),2) + pow(abs(value['AcX']),2));
+        difference = abs(self.previous_sample - norm)
+        self.previous_sample = norm
+
+        return difference
