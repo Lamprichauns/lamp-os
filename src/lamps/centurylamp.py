a51bb253b0e011bba800910694abd541b2eaf33a
Rich <chaffneue@gmail.com>
2022-04-28 01:28:43 -0700

    adding a light effect when a certain motion limit is passed

--- src/lamps/centurylamp.py
+++ src/lamps/centurylamp.py
@@ -2,26 +2,50 @@ from lamp import Lamp
 from behaviour import Behaviour
 from motion_6050 import LampMotionMPU6050
 import uasyncio as asyncio
+import random
 
 config = {
     "base": { "pin": 12, "pixels": 40 },
     "shade": { "pin": 13, "pixels": 40 },
-    "motion": { "pin_sda": 21, "pin_scl": 22, "polling_interval": 10e-2 }
+    "motion": { "pin_sda": 21, "pin_scl": 22, "polling_interval": 10e-2, "peak_hold_interval": 3, "walk_gesture_peak": 3000, "dance_gesture_peak": 15000 }
 }
 
 motion = LampMotionMPU6050(config["motion"]["pin_sda"], config["motion"]["pin_scl"])
 
 # Sample punter motion
 class MotionCaptureBehavior(Behaviour):
-    def measure(self):
-        print(motion.get_movement_intensity_value())
+    async def measure(self):
+        value = motion.get_movement_intensity_value()
+        if (value >= self.dance_gesture_peak):
+            print("dancing");
+            pixel = random.choice(range(0,self.lamp.shade.num_pixels-10))
+            pixel2 = random.choice(range(0,self.lamp.shade.num_pixels-10))
+
+            current_pixels = list(self.lamp.shade.pixels)
+
+            new_pixels = current_pixels.copy()
+
+            # Quick flash to white
+            new_pixels[pixel] = (250,250,255)
+            new_pixels[pixel2] = (250,250,255)
+            await self.lamp.shade.async_fade(new_pixels,80,2)
+
+            # Quick flash to black
+            new_pixels[pixel] = (0,0,0)
+            new_pixels[pixel2] = (0,0,0)
+            await self.lamp.shade.async_fade(new_pixels,100,5)
+
+            # Slow fade back to before
+            await self.lamp.shade.async_fade(current_pixels,200,5)
 
     async def run(self):
         polling_interval = config["motion"]["polling_interval"]
+        self.dance_gesture_peak = config["motion"]["dance_gesture_peak"];
+        self.walk_gesture_peak = config["motion"]["walk_gesture_peak"];
         while True:
             await asyncio.sleep(polling_interval)
             async with self.lamp.lock:
-                self.measure()
+                await self.measure()
 
 century = Lamp("century", "#5b4711", "#5b4711", config)
 century.add_behaviour(MotionCaptureBehavior)
