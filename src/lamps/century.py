14015d88c1a1cb27364475e7ea0e33074c931aa1
Rich <chaffneue@gmail.com>
2022-05-02 23:42:16 -0700

    Introduces century lamp which uses the motion sensor to change the base color (temp and acceleration) (currently assigned to shade for testing). Adds some helpful utils for darkening and brightening pixels as well as making a gradient fill

---
+++ src/lamps/century.py
@@ -0,0 +1,149 @@
+# An example of a completely century lamp
+import random
+from math import ceil
+import uasyncio as asyncio
+from behaviours.defaults import LampFadeIn
+from lamp_core.custom_lamp import CustomLamp
+from lamp_core.behaviour import Behaviour
+from components.led.led_strip_2812_rgb import LedStrip2812RGB
+from components.motion.motion_6050 import MotionMPU6050
+from components.network.bluetooth import Bluetooth
+from components.temperature.temperature_6050 import TemperatureMPU6050
+from utils.color_gradient import create_gradient
+from utils.color_brighten import brighten
+from utils.color_darken import darken
+
+# for ease of use, you can define a config to flow into all the components
+config = {
+    "lamp": { "name": "century" },
+    "shade": { "pin": 13, "pixels": 40, "default_color": "#931702" },
+    "base": { "pin": 12, "pixels": 5, "default_color": "#270000" },
+    "motion": { "pin_sda": 21, "pin_scl": 22},
+    "dance_reaction": {"polling_interval": 50, "dance_gesture_peak": 20000}
+}
+
+# This behavior will create a very slow cooling/warming color change in reaction the ambient temperature
+class Sunset(Behaviour):
+    def __init__(self, lamp):
+        super().__init__(lamp)
+        self.sunset_stages = [
+            { "color_start":(138, 15, 3, 0), "color_end": (220, 60, 8, 0), "clouds": False, "stars": False },
+            { "color_start":(108, 13, 3, 0), "color_end": (217, 68, 30, 0), "clouds": False, "stars": False },
+            { "color_start":(108, 4, 23, 0), "color_end": (181, 96, 14, 0), "clouds": True, "stars": False },
+            { "color_start":(107, 5, 57, 0), "color_end": (155, 180, 14, 0), "clouds": True, "stars": False },
+            { "color_start":(52, 4, 107, 0), "color_end": (104, 180, 15, 0), "clouds": True, "stars": True },
+            { "color_start":(16, 7, 142, 0), "color_end": (85, 145, 16, 0), "clouds": True, "stars": True },
+            { "color_start": (5, 33, 90, 0), "color_end": (30, 112, 12, 0), "clouds": True, "stars": True },
+        ]
+        self.current_scene = 0
+        self.polling_interval = 10
+
+    def get_scene_for_temperature(self):
+        temperature = self.lamp.temperature.get_temperature_value()
+        print(temperature)
+
+        if temperature > 25:
+            return 0
+        if temperature > 22:
+            return 1
+        if temperature > 18:
+            return 2
+        if temperature > 16:
+            return 3
+        if temperature > 14:
+            return 4
+        if temperature > 12:
+            return 5
+        return 6
+
+    def draw_scene(self, scene):
+        new_pixels = create_gradient(self.sunset_stages[scene]["color_start"], self.sunset_stages[scene]["color_end"], steps=40)
+
+        for k in range(20,25):
+            new_pixels[k] = brighten(self.lamp.shade.default_pixels[k], percentage=15)
+
+        if self.sunset_stages[scene]["clouds"] is True:
+            new_pixels[random.choice(range(19, 28))] = (255, 0, 210, 0)
+            new_pixels[random.choice(range(18, 25))] = (230, 0, 110, 0)
+            new_pixels[random.choice(range(20, 27))] = (212, 0, 178, 0)
+
+        if self.sunset_stages[scene]["stars"] is True:
+            new_pixels[random.choice(range(25, 40))] = (250, 250, 250, 0)
+            new_pixels[random.choice(range(27, 40))] = (250, 250, 250, 0)
+
+        for k in range(31,37):
+            new_pixels[k] = darken(self.lamp.shade.default_pixels[k], percentage=85)
+
+        return new_pixels
+
+    async def run(self):
+        while True:
+            await asyncio.sleep(self.polling_interval)
+            scene = self.get_scene_for_temperature()
+            if scene != self.current_scene:
+                self.current_scene = scene
+                await self.lamp.shade.fade(self.draw_scene(scene), 150, 500)
+
+# century: Animate when lamp is under higher than normal acceleration to use as a rave scepter
+class DanceReaction(Behaviour):
+    def __init__(self, lamp):
+        super().__init__(lamp)
+        self.last_accelerometer_value = 0
+        self.polling_interval = config["dance_reaction"]["polling_interval"]
+        self.dance_gesture_peak = config["dance_reaction"]["dance_gesture_peak"]
+
+    async def measure(self):
+        value = self.lamp.motion.get_movement_intensity_value()
+        if (value >= self.dance_gesture_peak and value is not self.last_accelerometer_value):
+            self.last_accelerometer_value = value
+            pixel_list = [random.randrange(0, self.lamp.shade.num_pixels, 1) for i in range(ceil(self.lamp.shade.num_pixels/2))]
+            current_pixels = list(self.lamp.shade.pixels)
+            new_pixels = current_pixels.copy()
+
+            # Quick flash to white
+            for i in pixel_list:
+                new_pixels[i] = (250, 250, 255, 255)
+            await self.lamp.shade.fade(new_pixels, 10, 2)
+
+            # Quick flash to black
+            for i in pixel_list:
+                new_pixels[i] = (0, 0, 0, 0)
+            await self.lamp.shade.fade(new_pixels, 5, 5)
+
+            # Slow fade back to before
+            await self.lamp.shade.fade(current_pixels, 10, 5)
+
+            # Don't trigger again for a while
+            await asyncio.sleep_ms(500)
+
+    async def run(self):
+        while True:
+            await asyncio.sleep_ms(self.polling_interval)
+            async with self.lamp.lock:
+                await self.measure()
+
+# Compose components and behaviours to your liking
+century = CustomLamp(config["lamp"]["name"])
+century.shade = LedStrip2812RGB(config["shade"]["default_color"], config["shade"]["pin"], config["shade"]["pixels"])
+century.base = LedStrip2812RGB(config["base"]["default_color"], config["base"]["pin"], config["base"]["pixels"])
+century.motion = MotionMPU6050(config["motion"]["pin_sda"], config["motion"]["pin_scl"])
+century.temperature = TemperatureMPU6050(century.motion.accelerometer)
+century.bluetooth = Bluetooth(config["lamp"]["name"], config["base"]["default_color"], config["shade"]["default_color"])
+century.network = century.bluetooth.network
+century.bluetooth.enable()
+
+# Programatically create a gradient for each step in the sunset
+century.shade.default_pixels = create_gradient((138, 15, 3, 0), (220, 60, 8, 0), steps=40)
+
+for j in range(31,37):
+    century.shade.default_pixels[j] = darken(century.shade.default_pixels[j], percentage=85)
+for j in range(20,25):
+    century.shade.default_pixels[j] = brighten(century.shade.default_pixels[j], percentage=15)
+
+print(century.shade.default_pixels[39])
+
+century.add_behaviour(LampFadeIn)
+century.add_behaviour(DanceReaction)
+century.add_behaviour(Sunset)
+
+century.wake()
