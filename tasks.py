532700fbd6a5ad254f243ecc803d04776e06582a
Rich <chaffneue@gmail.com>
2023-06-28 22:09:37 -0700

    More cleanup: upgrading esp32 core to remove glitching from animations. removing unused files to save flash space. removing old tasks tooling

--- tasks.py
+++ tasks.py
@@ -12,7 +12,7 @@ def erase(c, port):
 
 @task
 def flash(c, port):
-    c.run(f"esptool.py --chip esp32 --port {port} write_flash -z 0x1000 esp32-20220618-v1.19.1.bin")
+    c.run(f"esptool.py --chip esp32 --port {port} write_flash -z 0x1000 esp32-20230426-v1.20.0.bin")
 
 @task
 def upload(c, port): 
