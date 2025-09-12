cd590af6204bae1dfe8385271ec0accd0dc04bf2
Rich <chaffneue@gmail.com>
2025-09-11 19:47:04 -0700

    Removing python from mainline - updating docs

--- src/behaviours/lamp_idle.py
+++
@@ -1,8 +0,0 @@
-from lamp_core.behaviour import AnimatedBehaviour
-
-class LampIdle(AnimatedBehaviour):
-    async def draw(self):
-        self.lamp.shade.buffer = self.lamp.shade.default_pixels.copy()
-        self.lamp.base.buffer = self.lamp.base.default_pixels.copy()
-
-        await self.next_frame()
--- src/utils/hex_to_rgbw.py
+++
@@ -1,7 +0,0 @@
-# Convert hex colors to RGBW - Automatically flip full white to 0,0,0,255 (turn on warm white led
-# instead of each individual color)
-def hex_to_rgbw(value):
-    value = value.lstrip('#')
-    rgb = tuple(int(value[i:i+2], 16) for i in (0, 2, 4))
-
-    return (0,0,0,255) if rgb == (255,255,255) else rgb + (0,)
